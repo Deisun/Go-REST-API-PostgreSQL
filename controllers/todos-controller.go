407f7445d66aaa0f01741ca45e76b2f03b10fcf4
Deisun <rdouma81@gmail.com>
2020-09-30 21:08:25 -0400

    Implemented Delete and Update API

--- controllers/todos-controller.go
+++ controllers/todos-controller.go
@@ -4,24 +4,26 @@ import (
 	"awesomeProject1/database"
 	"awesomeProject1/models"
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"strconv"
 )
 
-func GetAllTodos(w http.ResponseWriter, req *http.Request) {
+var todos []models.Todo
+
+func GetAllTodos(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Only GET requests allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
 	rows, err := database.DB.Query("SELECT * FROM todos")
 	if err != nil {
-		fmt.Println("could not do DB QUERY")
-		fmt.Println(err)
+		http.Error(w, "Unable to query DB", http.StatusBadRequest)
 		return
 	}
 	defer rows.Close()
-
-	var todos []models.Todo
 
 	for rows.Next() {
 
@@ -42,8 +44,7 @@ func GetAllTodos(w http.ResponseWriter, req *http.Request) {
 }
 
 func GetTodo(w http.ResponseWriter, r *http.Request) {
-	idParam := mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idParam)
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
 		log.Println("Problem converting integer")
 	}
@@ -61,5 +62,72 @@ func GetTodo(w http.ResponseWriter, r *http.Request) {
 	err = json.NewEncoder(w).Encode(todo)
 	if err != nil {
 		http.Error(w, "Problem encoding", http.StatusTeapot)
+	}
+}
+
+func CreateTodo(w http.ResponseWriter, r *http.Request) {
+	todo := models.Todo{}
+	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("The data sent was bad"))
+		return
+	}
+
+	stmt, err := database.DB.Prepare("INSERT INTO todos (name) VALUES ($1)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close()
+
+	if _, err := stmt.Exec(todo.Name); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		log.Println("there was a problem encoding")
+	}
+}
+
+func UpdateTodo(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	var todo models.Todo
+	err = json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := database.DB.Prepare("UPDATE todos SET name = $2 WHERE todos_id = $1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close()
+
+	if _, err = stmt.Exec(id, todo.Name); err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-type", "application/json")
+}
+
+func RemoveTodo(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		log.Println(err)
+	}
+
+	stmt, err := database.DB.Prepare("DELETE FROM todos WHERE todos_id = $1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	if _, err = stmt.Exec(id); err != nil {
+		log.Println(err)
 	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,4 +9,7 @@ var RegisterRoutes = func(router *mux.Router) {
 
 	router.HandleFunc("/api/todos", controllers.GetAllTodos).Methods("GET")
 	router.HandleFunc("/api/todos/{id}", controllers.GetTodo).Methods("GET")
+	router.HandleFunc("/api/todos/create", controllers.CreateTodo).Methods("POST")
+	router.HandleFunc("/api/todos/update/{id}", controllers.UpdateTodo).Methods("PUT")
+	router.HandleFunc("/api/todos/delete/{id}", controllers.RemoveTodo).Methods("DELETE")
 }
